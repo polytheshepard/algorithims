b723f63a75c14b28d305c3a3f91847bb18ed4359
Katies Truong <s3671053@student.rmit.edu.au>
2017-10-14 20:09:55 +1100

    Updated algorithims

---
+++ reversestring.js
@@ -0,0 +1,9 @@
+// Reverse the provided string.
+// You may need to turn the string into an array before you can reverse it.
+// You result must be a string.
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString('Hello'));
