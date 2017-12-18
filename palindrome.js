fb3521b21b77a2ff1868cd52e4a1b56855ce91df
polytheshepard <katies.truong@gmail.com>
2017-12-18 14:55:38 +1100

    Update palindrome.js
    Adding in comment explanation.

--- palindrome.js
+++ palindrome.js
@@ -10,7 +10,7 @@
 // We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
 
 function palidrome(str) {
-  var replace = str.replace(/[/\W/_]/g, "").toLowerCase();
+  var replace = str.replace(/[/\W/_]/g, "").toLowerCase(); // Matches non-alpabetic characters and replaces with empty string and lower cases any characters.
   var reverse = replace.split("").reverse().join("");
   if (replace === reverse) {
     return true;
