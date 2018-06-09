9f4683d3617b79a83ef33c79cc1dc6a58d3bf033
Katies Truong <katies.truong@gmail.com>
2018-06-09 19:06:54 +1000

    Removed .js files

--- reversestring.js
+++
@@ -1,9 +0,0 @@
-// Reverse the provided string.
-// You may need to turn the string into an array before you can reverse it.
-// You result must be a string.
-
-function reverseString(str) {
-  return str.split('').reverse().join('');
-}
-
-console.log(reverseString('Hello'));
